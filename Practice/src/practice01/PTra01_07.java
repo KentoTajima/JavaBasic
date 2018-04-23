daa295ec3f11970c1ce5d741ad5e143c1b7a9e9a
tajimakento <tajiken1214@gmail.com>
2018-04-23 18:08:50 +0900

    Practice01-02~03-09まで

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,24 +21,24 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
-
-
-
+		bo=true;
+		by=5;
+		s=7;
+		c='a';
+		i=100;
+		l=100092909L;
+		f=1.5F;
+		d=2.4;
 
 		// ★ それぞれの変数の中身を出力してください
-
-
-
-
-
-
-
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 	}
 }
--- Practice/src/practice03/PTra03_09.java
+++ Practice/src/practice03/PTra03_09.java
@@ -18,5 +18,44 @@ public class PTra03_09 {
 		 *
 		 * ※ プログラムは何行書いても良いです
 		 */
+	java.util.Scanner scanner = new java.util.Scanner(System.in) ;
+
+	System.out.println("0-6を選択してください");
+	System.out.println("------------------------------------------");
+	System.out.println("*　0.日曜");
+	System.out.println("*　1.月曜");
+	System.out.println("*　2.火曜");
+	System.out.println("*　3.水曜");
+	System.out.println("*　4.木曜");
+	System.out.println("*　5.金曜");
+	System.out.println("*　6.土曜");
+	System.out.println("------------------------------------------");
+
+	int num = scanner.nextInt();
+
+	if(num == 0) {
+		System.out.println("日曜");
+
+	}if(num == 1) {
+		System.out.println("月曜");
+
+	}if(num == 2) {
+		System.out.println("火曜");
+
+	}if(num == 3) {
+		System.out.println("水曜");
+
+	}if(num == 4) {
+		System.out.println("木曜");
+
+	}if(num == 5) {
+		System.out.println("金曜");
+
+	}if(num == 6) {
+		System.out.println("土曜");
+
+	}else if(num<0||num>6){
+		System.out.println("0～6の整数を入力してください");
 	}
-}
+	}
+   }
