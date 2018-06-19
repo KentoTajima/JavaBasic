67deab9eacb47d2bfaa3a82f4befd064e8cd4542
tajimakento <tajiken1214@gmail.com>
2018-06-19 17:47:04 +0900

    Ptra10_04まで完了

--- Practice/src/practice10/PTra10_01.java
+++ Practice/src/practice10/PTra10_01.java
@@ -1,10 +1,4 @@
 package practice10;
-/*
- * PTra10_01.java
- *   作成	LIKEIT	2017
- *------------------------------------------------------------
- * Copyright(c) Rhizome Inc. All Rights Reserved.
- */
 
 public class PTra10_01 {
 
@@ -21,16 +15,31 @@ public class PTra10_01 {
 	 * メソッド
 	 * 		なし
 	 */
+	
+
+	
 
 	public static void main(String[] args) {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
 
+		User us;
+		
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
 
+		us=new User();
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
 
+		
+		System.out.println(us.userId);
+		
+		System.out.println(us.userNm);
+		
+		System.out.println(us.mail);
+		
+		System.out.println(us.password);
+		
 	}
 }
